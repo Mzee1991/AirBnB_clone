2d82bc725d2b4c6619cc9041b678756cc5299dad
astonetuhame <astonetuhame@gmail.com>
2022-08-07 19:04:56 +0300

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,17 +1,4 @@
-#!/usr/bin/python3
-"""
-initialize the models package
-"""
-
-from os import getenv
-
-
-storage_t = getenv("HBNB_TYPE_STORAGE")
-
-if storage_t == "db":
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
+"""Module for FileStorage autoinit."""
+from models.engine.file_storage import FileStorage
+storage = FileStorage()
 storage.reload()
